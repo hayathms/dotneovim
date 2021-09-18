bc934e93030f22cfa5a42eaac1763e73d0feea69
Meir Kriheli <mkriheli@gmail.com>
2021-09-18 23:09:51 +0300

    Set columnwitdh for rust to 100
    To match RustFmt

--- plugin/programming.lua
+++ plugin/programming.lua
@@ -1,5 +1,8 @@
 -- Auto reformat Rust files on save
 vim.g.rustfmt_autosave = 1
 
+-- RustFmt uses 100 for line width
+vim.api.nvim_command('autocmd bufreadpre *.rs setlocal colorcolumn=100')
+
 -- Enable colorizer plugin
 require'colorizer'.setup()
