dda65de275496c8273e8b4395dd6d9f11db97bc6
Meir Kriheli <mkriheli@gmail.com>
2022-06-06 02:01:04 +0300

    Added key mappings

---
+++ lua/keymaps.lua
@@ -0,0 +1,56 @@
+------------------------------------
+-- key mapppings
+------------------------------------
+
+local map = vim.api.nvim_set_keymap             -- shortcut to the map func
+local opts = { noremap = true, silent = true }  -- DRY for map options
+local expr = { noremap = true, silent = true, expr = true }  -- Opts and expr
+
+-- map the leader key to space
+map('n', '<Space>', '', {})
+vim.g.mapleader = ' '                           -- 'vim.g' sets global variables
+
+
+-- Maps for jj to act as Esc in insert and command modes
+map('i', 'jj', '<ESC>', opts)
+map('c', 'jj', '<ESC>', opts)
+
+-- Smarter j/k navigation
+-- Convert the j and k movement commands from strict linewise movements to
+-- onscreen display line movements via the gj and gk commands. When
+-- preceded with a count we want to go back to strict linewise movements.
+-- will automatically save movements larger than 5 lines to the jumplist.
+map('n', 'j', 'v:count ? (v:count > 5 ? "m\'" . v:count : "") . "j" : "gj"', expr)
+map('n', 'k', 'v:count ? (v:count > 5 ? "m\'" . v:count : "") . "k" : "gk"', expr)
+
+-- Center next/previous matched string
+map('n', 'n', 'nzz', opts)
+map('n', 'N', 'Nzz', opts)
+
+-- control + vim direction key to navigate windows
+map('n', '<C-J>', '<C-W>j', opts)
+map('n', '<C-K>', '<C-W>k', opts)
+map('n', '<C-H>', '<C-W>h', opts)
+map('n', '<C-L>', '<C-W>l', opts)
+
+-- Make these all work in insert mode
+map('i', '<C-W>', '<C-O><C-W>', opts)
+
+-- control + arrow key to resize
+map('n', '<C-Down>',  '<C-W>+', opts)
+map('n', '<C-Up>',    '<C-W>-', opts)
+map('n', '<C-Left>',  '<C-W><', opts)
+map('n', '<C-Right>', '<C-W>>', opts)
+
+
+-- Navigation from terminal
+map("t", "<C-h>", "<C-\\><C-N><C-w>h", opts)
+map("t", "<C-j>", "<C-\\><C-N><C-w>j", opts)
+map("t", "<C-k>", "<C-\\><C-N><C-w>k", opts)
+map("t", "<C-l>", "<C-\\><C-N><C-w>l", opts)
+
+-- <leader>V Activate changes to init.lua (needs save)
+map('n', '<leader>V', ':source ~/.config/nvim/init.lua<CR>', opts)
+
+-- Clean all end of line whitespace with <Leader>S
+map('n', '<leader>S', [[:let _s=@/<Bar>:%s/\s\+$//e<Bar>:let @/=_s<Bar>:nohl<CR>]], opts)
