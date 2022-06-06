cd23d4f9528e9d2c704cf817d59d180528876746
Meir Kriheli <mkriheli@gmail.com>
2022-06-06 08:29:01 +0300

    Added completion

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -53,6 +53,20 @@ return packer.startup(function(use)
   -- colorschemes
   use "shaunsingh/nord.nvim"
 
+  -- Completion
+  use "hrsh7th/nvim-cmp"
+  use "hrsh7th/cmp-buffer"
+  use "hrsh7th/cmp-nvim-lsp"
+  use "hrsh7th/cmp-emoji"
+  use "hrsh7th/cmp-path"
+  use "hrsh7th/cmp-nvim-lua"
+  use "f3fora/cmp-spell"
+  use "saadparwaiz1/cmp_luasnip"
+  --
+    -- snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
